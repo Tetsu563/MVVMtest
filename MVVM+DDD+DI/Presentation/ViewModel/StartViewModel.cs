f544399adeffbf05894a284b57d31ce568334bcd
Tetsu563 <tetsucoro7@gmail.com>
2025-06-26 01:13:39 +0900

    StartViewModel追加

---
+++ MVVM+DDD+DI/Presentation/ViewModel/StartViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_DDD_DI.Presentation.ViewModel
+{
+    class StartViewModel
+    {
+    }
+}
